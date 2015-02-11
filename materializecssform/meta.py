1c6f379f5203fa86b4e1580d0dbe2dd57175e10e
flow <clapieflor@eisti.eu>
2015-02-11 15:32:35 +0100

    init for package

--- materializecssform/meta.py
+++ materializecssform/meta.py
@@ -1,5 +1,5 @@
 from distutils.version import StrictVersion
 
 
-VERSION = StrictVersion('0.96')
+VERSION = StrictVersion('1.0.0')
 
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+ 
+from setuptools import setup, find_packages
+ 
+import materializecssform
+ 
+setup(
+ 
+    name='django-materializecss-form',
+ 
+    version=materializecssform.__version__,
+ 
+    packages=find_packages(),
+ 
+    author="Florent CLAPIÉ",
+ 
+    author_email="clapie.florent@gmail.com",
+ 
+    description="A simple Django form template tag to work with Materializecss",
+
+    long_description=open('README.md').read(),
+ 
+    # install_requires= ,
+ 
+    include_package_data=True,
+ 
+    url='https://github.com/florent1933/django-materializecss-form',
+ 
+    classifiers=[
+        "Programming Language :: Python",
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Documentation :: Sphinx",
+    ],
+ 
+    license="MIT",
+)
