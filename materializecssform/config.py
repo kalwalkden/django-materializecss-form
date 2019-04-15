363d23742c494c370e243ce0f25291c0b2391780
Johan Niklasson <johan@nicklassons.se>
2019-04-15 20:27:48 +0200

    Added config setting for icon set

--- materializecssform/config.py
+++ materializecssform/config.py
@@ -2,3 +2,4 @@ from django.conf import settings
 
 MATERIALIZECSS_COLUMN_COUNT = getattr(settings, 'MATERIALIZECSS_COLUMN_COUNT', 12)
 MATERIALIZECSS_VALIDATION = getattr(settings, 'MATERIALIZECSS_VALIDATION', True)
+MATERIALIZECSS_ICON_SET = getattr(settings, 'MATERIALIZECSS_ICON_SET', 'default')
